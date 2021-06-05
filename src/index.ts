9866ea4026510310280d4f4eb6a3065b82558ba3
Nam Giang <kyng@ece.ubc.ca>
2021-06-05 07:10:41 -0700

    lint

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import Jimp from 'jimp/es';
 import Path from 'path';
 
 const dir = process.env.DIRNAME || `${__dirname}/../`;
-const fontPath = Path.join(
-  dir,
-  'assets/fonts/sans_white/sans_white.fnt'
-);
+const fontPath = Path.join(dir, 'assets/fonts/sans_white/sans_white.fnt');
 const gradientPath = Path.join(dir, 'assets/gradient.png');
 const curiocityPath = Path.join(dir, 'assets/logos/curiocity.png');
 
@@ -13,25 +10,25 @@ const width = 1920;
 const height = 1080;
 
 export const generateNewsCreative = async (
-  enclosureUrl: string, 
+  enclosureUrl: string,
   text: string,
 ): Promise<Jimp> => {
-
   const font = await Jimp.loadFont(fontPath);
   const gradient = (await Jimp.read(gradientPath)).resize(width, height);
   const logo = (await Jimp.read(curiocityPath)).resize(345, 73);
   const enclosure = await Jimp.read(enclosureUrl);
 
   let effectiveText = text;
-  if (text.length >= 77){
-    effectiveText = text.substring(0, 74) + "...";
+  if (text.length >= 77) {
+    effectiveText = text.substring(0, 74) + '...';
   }
 
-  enclosure.resize(width, height)
-  .composite(gradient, 0, 0)
-  .composite(logo, 120, 120)
-  .print(font, 120, 270, effectiveText, 1400)
-  .quality(50);
+  enclosure
+    .resize(width, height)
+    .composite(gradient, 0, 0)
+    .composite(logo, 120, 120)
+    .print(font, 120, 270, effectiveText, 1400)
+    .quality(50);
 
   return enclosure;
-}
+};
--- test/generator.test.ts
+++ test/generator.test.ts
@@ -2,10 +2,12 @@ import { generateNewsCreative } from '../src';
 
 describe('generateNewsCreative', () => {
   it('generateNewsCreative', async () => {
-    const enclosureUrl = "https://curiocity.com/wp-content/uploads/2021/06/bigpizza.jpg";
-    const text = "This GTA spot serves insanely big pizza slices you've got to see to believe, blaeoio awejfoiajwe oawej ofijawef";
+    const enclosureUrl =
+      'https://curiocity.com/wp-content/uploads/2021/06/bigpizza.jpg';
+    const text =
+      "This GTA spot serves insanely big pizza slices you've got to see to believe, blaeoio awejfoiajwe oawej ofijawef";
 
     const image = await generateNewsCreative(enclosureUrl, text);
-    image.write("bigpizza.jpeg");
+    image.write('bigpizza.jpeg');
   });
 });
